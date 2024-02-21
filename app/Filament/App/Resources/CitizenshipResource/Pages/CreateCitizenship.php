04c7e8f0cd4555731b8fb41b6bd7a92ce8de0946
Yismen Jorge <yismen.jorge@gmail.com>
2024-02-21 13:27:52 -0400

    add citicenzhip resource

---
+++ app/Filament/App/Resources/CitizenshipResource/Pages/CreateCitizenship.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\App\Resources\CitizenshipResource\Pages;
+
+use App\Filament\App\Resources\CitizenshipResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCitizenship extends CreateRecord
+{
+    protected static string $resource = CitizenshipResource::class;
+}
