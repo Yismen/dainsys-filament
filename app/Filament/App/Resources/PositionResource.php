5ab62a710dac949cbc29373688f50166db35978e
Yismen Jorge <yismen.jorge@gmail.com>
2024-02-21 13:54:41 -0400

    Add position resource

---
+++ app/Filament/App/Resources/PositionResource.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Filament\App\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Position;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\App\Resources\PositionResource\Pages;
+use App\Filament\App\Resources\PositionResource\RelationManagers;
+
+class PositionResource extends Resource
+{
+    protected static ?string $model = Position::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('department_id')
+                    ->relationship('department', 'name')
+                    ->autofocus()
+                    ->required()
+                    ->createOptionForm([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->autofocus(),
+                        Forms\Components\TextInput::make('description'),
+                    ]),
+                Forms\Components\Select::make('payment_type_id')
+                    ->relationship('paymentType', 'name')
+                    ->required()
+                    ->createOptionForm([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->autofocus(),
+                        Forms\Components\TextInput::make('description'),
+                    ]),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->unique()
+                    ->maxLength(500),
+                Forms\Components\TextInput::make('salary')
+                    ->required()
+                    ->minValue(0)
+                    ->numeric(),
+                Forms\Components\Textarea::make('description')
+                    ->maxLength(65535)
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('department.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('paymentType.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('salary')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPositions::route('/'),
+            'create' => Pages\CreatePosition::route('/create'),
+            'view' => Pages\ViewPosition::route('/{record}'),
+            'edit' => Pages\EditPosition::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/App/Resources/PositionResource/Pages/EditPosition.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\App\Resources\PositionResource\Pages;
+
+use App\Filament\App\Resources\PositionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPosition extends EditRecord
+{
+    protected static string $resource = PositionResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
