a3d99ca88eb6eda93d90b6a7d6ffa252c94835c0
Yismen Jorge <yismen.jorge@gmail.com>
2024-03-04 14:26:10 -0400

    Add role relationship manager

---
+++ app/Filament/Resources/RoleResource/RelationManagers/RolesRelationManager.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources\RoleResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\RelationManagers\RelationManager;
+
+class RolesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'roles';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->autofocus()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('guard_name')
+                    ->options([
+                        'web' => 'web',
+                        'api' => 'api',
+                    ])
+                    ->required(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('guard_name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+                Tables\Actions\AttachAction::make()
+                    ->preloadRecordSelect(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                // Tables\Actions\DeleteAction::make(),
+                Tables\Actions\DetachAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    // Tables\Actions\DeleteBulkAction::make(),
+                    // Tables\Actions\DetachAction::make(),
+                ]),
+            ]);
+    }
+}
