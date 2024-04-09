06db92a6fc5055b16703fbaf6f58772c0ee9ec4f
Yismen Jorge <yismen.jorge@gmail.com>
2024-04-09 13:16:28 -0400

    Refactor Employees count to a method

--- app/Filament/App/Widgets/HumanResource/EmployeesStats.php
+++ app/Filament/App/Widgets/HumanResource/EmployeesStats.php
@@ -4,6 +4,7 @@ namespace App\Filament\App\Widgets\HumanResource;
 
 use App\Models\Employee;
 use App\Enums\EmployeeStatus;
+use Illuminate\Database\Eloquent\Builder;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
@@ -17,47 +18,37 @@ class EmployeesStats extends BaseWidget
         return [
             Stat::make(
                 'Employees Active',
-                Employee::query()
-                    ->current()
-                    ->when($this->filters['site'], function ($employeeQuery) {
-                        $employeeQuery->whereHas('site', function ($siteQuery) {
-                            $siteQuery->when(
-                                is_array($this->filters['site']),
-                                fn ($query) => $query->whereIn('id', $this->filters['site']),
-                                fn ($query) => $query->where('id', $this->filters['site'])
-                            );
-                        });
-                    })
-                    ->count()
+                $this->employeesCount(scope: 'current')
             )
                 ->description('Employees that are currently working ')
                 ->color('success'),
             Stat::make(
                 'Employees Terminated',
-                Employee::query()
-                    ->inactive()
-                    ->when($this->filters['site'], function ($employeeQuery) {
-                        $employeeQuery->whereHas('site', function ($siteQuery) {
-                            $siteQuery->where('id', $this->filters['site']);
-                        });
-                    })
-                    ->count()
+                $this->employeesCount(scope: 'inactive')
             )
                 ->description('Employees that are no longer working with us')
                 ->color('danger'),
             Stat::make(
                 'Employees Suspended',
-                Employee::query()
-                    ->suspended()
-                    ->when($this->filters['site'], function ($employeeQuery) {
-                        $employeeQuery->whereHas('site', function ($siteQuery) {
-                            $siteQuery->where('id', $this->filters['site']);
-                        });
-                    })
-                    ->count()
+                $this->employeesCount(scope: 'suspended')
             )
                 ->description('Employees with a suspension reported')
                 ->color('warning'),
         ];
+    }
+
+    protected function employeesCount(string $scope): int
+    {
+        return Employee::query()
+            ->$scope()
+            ->when($this->filters['site'], function ($employeeQuery) {
+                $employeeQuery->whereHas('site', function ($siteQuery) {
+                    $siteQuery->when(
+                        is_array($this->filters['site']),
+                        fn ($query) => $query->whereIn('id', $this->filters['site']),
+                        fn ($query) => $query->where('id', $this->filters['site'])
+                    );
+                });
+            })->count();
     }
 }
