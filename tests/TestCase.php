82ff7f9ee36591468d7798e80d0680d75def5d1a
Yismen Jorge <yismen.jorge@gmail.com>
2025-03-13 10:43:34 -0400

    Add methods to give permissions to test users

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,36 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Foundation\Auth\User as AuthUser;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function createUserWithPermissionTo(string $permission): AuthUser
+    {
+        $user = User::factory()->create();
+
+        Permission::create(['name' => $permission]);
+        $user->givePermissionTo($permission);
+
+        return $user;
+    }
+
+    public function createUserWithPermissionsToActions(array $actions, string $model_name): AuthUser
+    {
+        $user = User::factory()->create();
+
+        foreach ($actions as $action) {
+            $permission = $action . ' ' . $model_name;
+
+            Permission::create(['name' => $permission]);
+            $user->givePermissionTo($permission);
+        }
+
+        return $user;
+    }
 }
