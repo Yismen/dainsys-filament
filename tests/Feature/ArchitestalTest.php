3817e77f996fd1a1bdd9d3d93c6e91e361ddc51c
Yismen Jorge <yismen.jorge@gmail.com>
2025-12-26 19:10:52 -0400

    Fixes scheduling command and updates model tests
    Updates the console kernel to properly schedule the telescope prune command.
    
    Enhances tests to ensure all models utilize soft deletes and includes UUIDs for specific models.
    
    Clarifies the expected traits for models, improving test coverage and accuracy.

--- tests/Feature/ArchitestalTest.php
+++ tests/Feature/ArchitestalTest.php
@@ -6,10 +6,14 @@
 |
 */
 
+use App\Models\Permission;
+use App\Models\Role;
+
 arch('all models should use soft deletes')
     ->expect('App\Models')
     ->toUseTraits([
         \Illuminate\Database\Eloquent\SoftDeletes::class,
+        \Illuminate\Database\Eloquent\Concerns\HasUuids::class,
     ])
     ->ignoring([
         \App\Models\Comment::class,
@@ -22,10 +26,11 @@ arch('all models should use ulids and factory')
     ->expect('App\Models')
     ->toUseTraits([
         \Illuminate\Database\Eloquent\Factories\HasFactory::class,
-        \Illuminate\Database\Eloquent\Concerns\HasUuids::class,
     ])
     ->ignoring([
         'App\Models\LiveVox',
         'App\Models\Services',
         'App\Models\Traits',
+        Permission::class,
+        Role::class,
     ]);
