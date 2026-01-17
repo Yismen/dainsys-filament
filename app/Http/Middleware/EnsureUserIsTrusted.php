4973c6823703919737b3fe508f5c63ccbe35ffe7
Yismen Jorge <yismen.jorge@gmail.com>
2026-01-16 20:57:14 -0400

    Remove unused Midleware files

---
+++ app/Http/Middleware/EnsureUserIsTrusted.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsTrusted
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        if(Auth::check() && ! Auth::user()->is_active) {
+            \abort(403, 'You are an inactive user. Contact the administrator!');
+        }
+
+        return $next($request);
+
+
+    }
+}
