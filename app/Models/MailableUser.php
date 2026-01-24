660b63417336aa4e660ded259812c6d723c63f86
Yismen Jorge <yismen.jorge@gmail.com>
2026-01-24 19:28:13 -0400

    Adds MailableUser model with relationships to Mailable and User

--- app/Models/MailableUser.php
+++ app/Models/MailableUser.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MailableUser extends Pivot
 {
-    //
+    protected $table = 'mailable_user';
+
+    public function mailable(): BelongsTo
+    {
+        return $this->belongsTo(Mailable::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
