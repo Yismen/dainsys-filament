24945ae9bc2832484971ebfba586239c5f0c7706
Yismen Jorge <yismen.jorge@gmail.com>
2026-01-24 07:12:48 -0400

    Delete unused files

--- resources/views/filament/app/pages/user-mailing-subscriptions.blade.php
+++
@@ -1,7 +0,0 @@
-<x-filament-panels::page>
-   {{-- <x-filament-panels::form wire:submit='syncMailables'>
-    {{ $this->ta }}
-    <x-filament-panels::form.actions :actions='$this->getFormActions()' />
-   </x-filament-panels::form> --}}
-   {{ $this->table }}
-</x-filament-panels::page>
