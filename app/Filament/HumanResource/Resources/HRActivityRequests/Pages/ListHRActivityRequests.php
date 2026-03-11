568857035cb94bcb2947d29879ca43dcce0be569
Yismen Jorge <yismen.jorge@gmail.com>
2026-03-11 00:12:29 -0400

    Remove option to create own requests for hr

--- app/Filament/HumanResource/Resources/HRActivityRequests/Pages/ListHRActivityRequests.php
+++ app/Filament/HumanResource/Resources/HRActivityRequests/Pages/ListHRActivityRequests.php
@@ -13,7 +13,7 @@ class ListHRActivityRequests extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            // CreateAction::make(),
         ];
     }
 }
