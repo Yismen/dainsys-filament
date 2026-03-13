b91b2ff044bb71f9dbf37a11a25de95c13743022
Yismen Jorge <yismen.jorge@gmail.com>
2026-03-13 17:11:34 -0400

    feat: Add FilamentJobsMonitor plugin and create migration for queue_monitors table

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,6 +6,7 @@ use AchyutN\FilamentLogViewer\FilamentLogViewer;
 use App\Services\FilamentPanelsService;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use BinaryBuilds\FilamentFailedJobs\FilamentFailedJobsPlugin;
+use Croustibat\FilamentJobsMonitor\FilamentJobsMonitorPlugin;
 use Filament\Navigation\NavigationItem;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -31,6 +32,7 @@ class AdminPanelProvider extends PanelProvider
                 FilamentSpatieLaravelBackupPlugin::make(),
             )
             ->plugins([
+                FilamentJobsMonitorPlugin::make(),
                 FilamentShieldPlugin::make()
                     ->navigationGroup('Access Control')
                     ->globallySearchable(true)
